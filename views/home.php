5700883a162426ca7d93104b79f985ae872831da
ramdane <ramdane221@unicaen.fr>
2024-11-12 09:19:23 +0100

    debut page home

--- views/home.php
+++ views/home.php
@@ -2,10 +2,31 @@
     <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="style/style.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Accueil - DungeonXplorer</title>
     </head>
     <body>
-        <h1>Bienvenue sur DungeonXplorer</h1>
+
+        <div class = "navbar">
+            <div class="menu_logo">
+                <a href="#" > <img src="image/Logo.png" alt="" class="logo"> </a>
+            </div>
+            <a href="#" class="bouton_connexion">connexion</a>
+            <a href="#" class="bouton_creer_compte">créer un compte</a>
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+        
     </body>
 </html>
